6a8d1e1157f78f7cd8da69d4d1ce8adeb3bee877
Andrei Ischenko <roobotik@gmail.com>
2017-04-13 23:47:32 +0300

    Ang 2
    test work

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'app',
+    templateUrl: 'app.components.html',
+    styleUrls: ['app.components.css']
+})
+
+export class AppComponent{
+    title = 'Angular 2Do'
+
+}
